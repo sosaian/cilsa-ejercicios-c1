b81c989eb2124891939cbe43e08eb27bdc02c08d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-14 17:10:49 -0300

    Add boilerplate, css reset and exercise

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+    MARK: EJERCICIOS CLASE 1
+
+    *   Crear un formulario de datos personales
+    *   Agregar CSS necesario para dar 2 estilos al formulario (normal y uno de "Alto Contraste")
+    *   Agregar atributos a inputs del formulario para campos obligatorios
+    *   Validar inputs del usuario en este formulario
+*/
