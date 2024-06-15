51c9e43d7c06c3a4ee809af4ff4d6f05a7b6a48d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-06-15 04:59:04 -0300

    Add high-contrast themed style to CSS with simple JS functionality for their respective buttons

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@
     *   Agregar atributos a inputs del formulario para campos obligatorios
     *   Validar inputs del usuario en este formulario
 */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const DEFAULT_STYLE_BUTTON = document.getElementById("styleDefault");
+
+    DEFAULT_STYLE_BUTTON.addEventListener("click", () => {
+        document.body.classList.remove("high_contrast")
+    });
+
+    const HIGH_CONTRAST_STYLE_BUTTON = document.getElementById("styleHighContrast");
+
+    HIGH_CONTRAST_STYLE_BUTTON.addEventListener("click", () => {
+        document.body.classList.add("high_contrast")
+    });
+});
